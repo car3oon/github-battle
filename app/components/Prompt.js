07fd2910b7cbdfc75a043fb2249faaf959fe4771
Łukasz Węgiel <lukasz@mirumee.com>
2017-01-08 10:33:22 +0100

    Refactor Prompt component

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react';
+import React, {PropTypes} from 'react';
 import styles from '../styles';
 
-class Prompt extends Component {
-  render() {
-    return (
+var Prompt = (props) => {
+  return (
       <div className="jumbotron col-sm-6 offset-sm-3 text-sm-center" style={styles.transparentBg}>
-        <h1>{this.props.header}</h1>
+        <h1>{props.header}</h1>
         <div className="col-sm-12">
-          <form onSubmit={this.props.onSubmitUser}>
+          <form onSubmit={props.onSubmitUser}>
             <div className="form-group">
               <input
                 className="form-control"
                 placeholder="Github Username"
-                onChange={this.props.onUpdateUser}
-                value={this.props.username}
+                onChange={props.onUpdateUser}
+                value={props.username}
                 type="text" />
             </div>
             <div className="form-group col-sm-4 offset-sm-4">
@@ -26,8 +25,14 @@ class Prompt extends Component {
           </form>
       </div>
     </div>
-    );
-  }
+  );
 }
+
+Prompt.propTypes = {
+    header: PropTypes.string.isRequired,
+    onSubmitUser: PropTypes.func.isRequired,
+    onUpdateUser: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired
+  }
 
 export default Prompt;
